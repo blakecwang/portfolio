655cb8baef115f66e77b5f583907dd55c908be47
Blake Wang <blakecwang@gmail.com>
2015-07-23 07:14:54 -0700

    categories setup

--- js/app.js
+++ js/app.js
@@ -5,27 +5,75 @@ var categories =
 [
 	{
 	"name": "Mechanical Engineering",
-	"selected": false
+	"content": meProjects,
+	"id": "me"
 	},
 	{
 	"name": "Web Development",
-	"selected": false
+	"content": weProjects,
+	"id": "we"
 	},
 	{
 	"name": "Graphic Design",
-	"selected": false
+	"content": grProjects,
+	"id": "gr"
+	},
+	{
+	"name": "Music",
+	"content": muProjects,
+	"id": "mu"
 	}
 ];
 
-$( "#mech" ).click(function() {
-  console.log( "Handler for .click() called." );
-});
+var meProjects = [
+	{
+	"name": "ROVA Pedestrian Assistive Technology",
+	"imgSrc": "some path",
+	"description": "bad-ass walker"
+	}
+];
+
+var weProjects = [
+	{
+	"name": "Frogger game",
+	"imgSrc": "some path",
+	"description": "bad-ass game"
+	}
+];
+
+var grProjects = [
+	{
+	"name": "String cheese people",
+	"imgSrc": "some path",
+	"description": "bad-ass drawing"
+	}
+];
+
+var muProjects = [
+	{
+	"name": "Got to Give",
+	"imgSrc": "some path",
+	"description": "bad-ass song"
+	}
+];
 
 
 //-----VIEWMODEL-----//
 
 var viewModel = function() {
-	this.currentCategory = ko.observable(-1);
+	var self = this;
+
+	this.currentCategory = ko.observable();
+
+	$( "#mech" ).click(function() {
+		self.currentCategory(0);
+	});
+	$( "#web" ).click(function() {
+		self.currentCategory(1);
+	});
+	$( "#graphic" ).click(function() {
+		self.currentCategory(2);
+	});
 };
 
 ko.applyBindings(new viewModel());
