31418aef6daf31f477c7a69cc6e0ac94f089ea28
Blake Wang <blakecwang@gmail.com>
2015-07-25 07:15:41 -0700

    change currentCat to numbers instead of objects

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // TODO
-// change currentCategory to int index instead of object
+// change currentCat to int index instead of object
 
 
 
@@ -68,55 +68,54 @@ var viewModel = function() {
 	var self = this;
 
 	// initialize some observables
-	this.categoryList = ko.observableArray();
-	this.currentCategory = ko.observable();
+	this.catList = ko.observableArray();
+	this.currentCat = ko.observable();
 
-	//populate categoryList
+	//populate catList
 	for (var k = 0; k < categories.length; k++) {
-		self.categoryList.push(categories[k]);
+		self.catList.push(categories[k]);
 	}
 
 	// define change current category function
-	this.changeCurrentCategory = function(category) {
-		self.currentCategory(category);
+	this.changeCurrentCat = function(catIndex) {
+		self.currentCat(catIndex);
 	};
 
 
 
-	this.initCategories = function(data) {
+	this.initCats = function(data) {
 
 		// add category elements to DOM
-		for (var i = 0; i < self.categoryList().length; i++) {
+		for (var i = 0; i < self.catList().length; i++) {
 
-				var catId = self.categoryList()[i].id;
-				var catName = self.categoryList()[i].name;
-				var elem = "<div class='row' "
+				var catId = self.catList()[i].id;
+				var catName = self.catList()[i].name;
+				var catElem = "<div class='row' "
 					+ "id='" + catId + "'>"
-					+ "<h2 data-bind='css: { selected: currentCategory() === " + i + "}'>"
+					+ "<h2 data-bind='css: { selected: currentCat() === " + i + "}'>"
 					+ catName + "</h2></div>";
 
-				$("#categories").append(elem);
+				$("#categories").append(catElem);
 
 		}
 
 		// add click listeners to category elements
-		for (var j = 0; j < self.categoryList().length; j++) {
+		for (var j = 0; j < self.catList().length; j++) {
 
-			var catObj = self.categoryList()[j];
+			var catObj = self.catList()[j];
 			var catIndex = j;
 			
 			(function(_data, _index) {
 				var catElem = "#" + _data.id;
 				$(catElem).click(function() {
-					self.changeCurrentCategory(_index);
+					self.changeCurrentCat(_index);
 				});
 			})(catObj, catIndex);
 		}
 	};
 
-	this.initCategories(self.categoryList());
+	this.initCats(self.catList());
 
-	this.changeCurrentCategory(self.categoryList()[1]);
 
 
 };
